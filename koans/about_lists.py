eda5d969150d9176719d95c42749ce19a43b2669
vermiilion <20220830@mywhitecliffe.com>
2022-09-14 20:26:54 +1200

    commit

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -11,7 +11,7 @@ class AboutLists(Koan):
     def test_creating_lists(self):
         empty_list = list()
         self.assertEqual(list, type(empty_list))
-        self.assertEqual(__, len(empty_list))
+        self.assertEqual(0, len(empty_list))
 
     def test_list_literals(self):
         nums = list()
@@ -21,29 +21,29 @@ class AboutLists(Koan):
         self.assertEqual([1], nums)
 
         nums[1:] = [2]
-        self.assertListEqual([1, __], nums)
+        self.assertListEqual([1, 2], nums)
 
         nums.append(333)
-        self.assertListEqual([1, 2, __], nums)
+        self.assertListEqual([1, 2, 333], nums)
 
     def test_accessing_list_elements(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[0])
-        self.assertEqual(__, noms[3])
-        self.assertEqual(__, noms[-1])
-        self.assertEqual(__, noms[-3])
+        self.assertEqual('peanut', noms[0])
+        self.assertEqual('jelly', noms[3])
+        self.assertEqual('jelly', noms[-1])
+        self.assertEqual('butter', noms[-3])
 
     def test_slicing_lists(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[0:1])
-        self.assertEqual(__, noms[0:2])
-        self.assertEqual(__, noms[2:2])
-        self.assertEqual(__, noms[2:20])
-        self.assertEqual(__, noms[4:0])
-        self.assertEqual(__, noms[4:100])
-        self.assertEqual(__, noms[5:0])
+        self.assertEqual('peanut', noms[0:1])
+        self.assertEqual('peanut', 'butter', noms[0:2])
+        self.assertEqual( , noms[2:2])
+        self.assertEqual('and', 'jelly', noms[2:20])
+        self.assertEqual( , noms[4:0])
+        self.assertEqual( , noms[4:100])
+        self.assertEqual( , noms[5:0])
 
     def test_slicing_to_the_edge(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
---
+++ koans/try:.py
@@ -0,0 +1,4 @@
+try:
+    None.some_method_none_does_not_know_about()
+except Exception as ex:
+    ex2 = ex
